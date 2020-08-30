200c7aa77383b759fb2a25c386c811c863e5d83e
Árni Benedikt Árnason <arnibarnason@gmail.com>
2020-08-30 13:20:06 +0000

    Change text to trigger build

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,4 +1,4 @@
 import React from "react";
 import styles from "./index.module.css";
 
-export const Hero = () => <div className={styles.heroContainer}>HERO</div>;
+export const Hero = () => <div className={styles.heroContainer}>TEKNIKAL</div>;
