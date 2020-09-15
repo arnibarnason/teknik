9184c639fb92c1a6d0439b76534d10f8b04b6351
Árni Benedikt Árnason <arnibarnason@gmail.com>
2020-09-15 00:10:27 +0000

    Style header and hero

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -3,7 +3,7 @@ import styles from "./index.module.css";
 
 export const Hero = () => (
   <div className={styles.heroContainer}>
-    <div className="verticalSpacing">
+    <div className={`verticalSpacing ${styles.contentContainer}`}>
       <h1 className={styles.title}>Við hjálpum þér að taka réttar ákvaðanir</h1>
       <h2 className={styles.subTitle}>
         Áhersla á virðisaukandi ráðgjöf og góða þjónustu við viðskiptavini
