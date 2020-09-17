a53fc2beae2d3fcd1317d648170dbdaa2c2da00f
Árni Benedikt Árnason <arnibarnason@gmail.com>
2020-09-17 11:21:39 +0000

    Page in progress message

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React from "react";
 import "./App.css";
-import { Menu } from "./components/Menu";
-import { Hero } from "./components/Hero";
-import { Content } from "./components/Content";
-import { Employees } from "./components/Employees";
-import { Footer } from "./components/Footer";
+// import { Menu } from "./components/Menu";
+// import { Hero } from "./components/Hero";
+// import { Content } from "./components/Content";
+// import { Employees } from "./components/Employees";
+// import { Footer } from "./components/Footer";
 
 function App() {
-  return (
-    <>
-      <Hero />
-      <Menu />
-      <Content />
-      <Employees />
-      <Footer />
-    </>
-  );
+  return <h1>Síða í vinnslu</h1>;
+  // return (
+  //   <>
+  //     <Hero />
+  //     <Menu />
+  //     <Content />
+  //     <Employees />
+  //     <Footer />
+  //   </>
+  // );
 }
 
 export default App;
